411be51a8b34a6893a8ceda8ac37fc0951f5a73a
unknown <nazih.developer@gmail.com>
2022-07-11 12:16:26 +0200

    Add map function in render

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,11 +3,12 @@ import React, { Component } from "react";
 class Product extends Component {
   render() {
     return (
-      <>
+      <div>
+        <button className="btn btn-primary m-2">5</button>
         <button className="btn btn-secondary m-2">+</button>
         <button className="btn btn-secondary m-2">-</button>
         <button className="btn btn-danger m-2">X</button>
-      </>
+      </div>
     );
   }
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,22 @@
 import { getProduct } from "../Services/fakeProducts";
 import React, { Component } from "react";
 import Product from "./Product";
+
 class Products extends Component {
-  state = getProduct;
+  state = { products: getProduct() };
 
   render() {
     return (
       <div>
-        <Product />
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              {this.state.products.map((product) => (
+                <Product product={product} />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
