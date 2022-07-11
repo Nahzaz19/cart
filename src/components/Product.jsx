ec2e4284ff39d8ba4a9285a63d61d2146074ed4d
unknown <nazih.developer@gmail.com>
2022-07-11 14:40:25 +0200

    Remake button features (more to do)

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,23 +6,22 @@ class Product extends Component {
       <div>
         <div class="container">
           <div class="row">
-            <div class="col-2">
-              <button className="btn btn-primary badge m-2">
+            <div class="col-1">
+              <button className="btn btn-primary badge fw-bold m-2">
                 {this.props.product.quantity}
               </button>
             </div>
             <div class="col">
-              {" "}
               <button
                 onClick={() => this.props.onIncrement(this.props.product)}
-                className="btn btn-secondary m-2"
+                className="btn btn-secondary fw-bold m-2"
               >
                 +
               </button>
-              <button className="btn btn-secondary m-2">-</button>
+              <button className="btn btn-secondary fw-bold m-">-</button>
               <button
                 onClick={() => this.props.onDelete(this.props.product.id)}
-                className="btn btn-danger m-2"
+                className="btn btn-danger fw-bold m-2"
               >
                 X
               </button>
